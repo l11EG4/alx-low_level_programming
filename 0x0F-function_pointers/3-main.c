69fc162f98cf8fcf15a91ab95efcfcca7bc66f58
l11EG4 <zakariaberahmi@gmail.com>
2023-04-19 10:55:25 -0700

    add files alx

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * by kilimini
+ *
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	int a, b;
+	char *op;
+	int (*operation)(int, int);
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(av[1]);
+	op = av[2];
+	if (op[1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	b = atoi(av[3]);
+	operation = get_op_func(op);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operation(a, b));
+	return (0);
+}
