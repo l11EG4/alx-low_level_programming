7623374ae8354058f214e281f5033f1ac267ccb1
l11EG4 <zakariaberahmi@gmail.com>
2023-06-14 22:12:16 -0700

    Mega

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - Deletes a node from a dlistint_t
+* @head: A pointer to the head of the dlistint_t
+* @index: The index of the node to delete
+* Made by MEGATRON
+* Return: Upon success or fails
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+	free(temp);
+	return (1);
+}
