2d522a911cb56adc9ac29639751bb19af1555a51
l11EG4 <zakariaberahmi@gmail.com>
2023-07-07 07:55:56 -0700

    Mega

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,21 +1,24 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_get - given key, get value
-* @ht: hashtable
-* @key: key
-* Made by MEGATRON
-* Return: value or zero if not found
-*/
-
+ * hash_table_get - given key, get value
+ * @ht: hash table
+ * @key: key
+ * Return: value; or NULL if not found
+ */
 char *hash_table_get(const table_t *ht, const char *key)
 {
-	ulint indx;
+	ulint idx;
 	node_t *tmp;
+
+	/* find index in hash table where key is */
+	/* look through linked list to find matching key for value */
+
 	if (!ht || !key)
 		return (NULL);
-	indx = key_index((const unsigned char *)key, ht->size);
-	tmp = (ht->array)[indx];
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	tmp = (ht->array)[idx];
 	while (tmp != NULL && strcmp(tmp->key, key) != 0)
 		tmp = tmp->next;
 	if (!tmp)
