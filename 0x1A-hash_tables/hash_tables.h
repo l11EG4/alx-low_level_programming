79eb1e074a987e403214e900ea83e1812dc9c438
l11EG4 <zakariaberahmi@gmail.com>
2023-07-07 07:17:05 -0700

    Mega

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,17 @@
+#ifndef __HASH_DEFS_H__
+#define __HASH_DEFS_H__
+
+/* Libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data.h"
+#include "100-init.h"
+
+/* Made by MEGATRON */
+
+#define table_t hash_table_t
+#define node_t hash_node_t
+
+#endif
