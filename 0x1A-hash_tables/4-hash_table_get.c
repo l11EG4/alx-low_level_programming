f08f918073def8ea5bf594e33510fe4c5ec17acb
l11EG4 <zakariaberahmi@gmail.com>
2023-07-07 07:52:03 -0700

    Mega

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,7 +15,6 @@ char *hash_table_get(const table_t *ht, const char *key)
 	if (!ht || !key)
 		return (NULL);
 	indx = key_index((const unsigned char *)key, ht->size);
-
 	tmp = (ht->array)[indx];
 	while (tmp != NULL && strcmp(tmp->key, key) != 0)
 		tmp = tmp->next;
