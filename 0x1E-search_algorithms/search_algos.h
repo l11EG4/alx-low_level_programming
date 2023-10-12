1fbda1d5732fb126411a3bee1d1c2c5b5822107f
l11EG4 <zakariaberahmi@gmail.com>
2023-10-11 19:20:03 -0700

    Mega

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,42 @@
 
 #define __local __attribute__((weak))
 
+/**
+* struct listint_s - singly linked list
+* @n: Integer.
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+* Made by MEGA
+* Description: singly linked list node structure
+* for ALX project
+*/
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+* struct skiplist_s - Singly linked list with an express lane
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+* @express: Pointer to the next node in the express lane
+* Made by MEGA
+* Description: singly linked list node structure with an express lane
+* for Holberton project
+*/
+
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 /*Func*/
 
 int linear_search(int *array, size_t size, int value);
@@ -15,6 +51,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 /**
 * print_array - prints an array
